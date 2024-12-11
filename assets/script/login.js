f44cae763a3b6ef18d338d7da7f1af6b8e910319
fatahillah2606 <muhamad.fatahillah@raharja.info>
2024-12-11 15:09:46 +0700

    Login page fixed

--- assets/script/login.js
+++ assets/script/login.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('.container');
 const resgisterBtn = document.querySelector('.register-btn');
-const loginBtn = document.querySelector('.');
+const loginBtn = document.querySelector('.login-btn');
 
 resgisterBtn.addEventListener('click', () => {
     container.classList.add('active');
