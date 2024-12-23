4631e99ea71cfa96c5fb067aa6044390427da19d
rezdni <reza.dani@raharja.info>
2024-12-23 12:53:37 +0700

    update

---
+++ db_sekolah.sql
@@ -0,0 +1,282 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 15 Des 2024 pada 08.43
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.1.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_sekolah`
+--
+CREATE DATABASE IF NOT EXISTS `db_sekolah` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `db_sekolah`;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `calon_siswa`
+--
+
+DROP TABLE IF EXISTS `calon_siswa`;
+CREATE TABLE `calon_siswa` (
+  `id_calon` int(11) NOT NULL,
+  `nama_calon_siswa` varchar(255) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `no_nis` int(20) NOT NULL,
+  `jenis_kelamin` enum('Laki-laki','Perempuan') NOT NULL,
+  `agama` enum('Islam','Katolik','Kristen Protestan','Hindu','Budha','Konghucu') NOT NULL,
+  `sekolah_asal` varchar(50) NOT NULL,
+  `kewarganegaraan` varchar(50) NOT NULL,
+  `golongan_darah` varchar(10) NOT NULL,
+  `alamat_tinggal` text NOT NULL,
+  `provinsi` varchar(50) NOT NULL,
+  `kota_kabupaten` varchar(50) NOT NULL,
+  `kecamatan` varchar(50) NOT NULL,
+  `kelurahan` varchar(50) NOT NULL,
+  `kode_post` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `dokumen_pendaftaran`
+--
+
+DROP TABLE IF EXISTS `dokumen_pendaftaran`;
+CREATE TABLE `dokumen_pendaftaran` (
+  `id_dokumen` int(11) NOT NULL,
+  `id_calon` int(11) NOT NULL,
+  `jenis_dokumen` varchar(255) NOT NULL,
+  `file_path` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `orang_tua`
+--
+
+DROP TABLE IF EXISTS `orang_tua`;
+CREATE TABLE `orang_tua` (
+  `id_orang_tua` int(11) NOT NULL,
+  `id_calon` int(11) NOT NULL,
+  `nama_orang_tua` varchar(255) NOT NULL,
+  `nomor_telepon_orang_tua` varchar(15) NOT NULL,
+  `pekerjaan_orang_tua` varchar(50) NOT NULL,
+  `alamat_orang_tua` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pendaftaran`
+--
+
+DROP TABLE IF EXISTS `pendaftaran`;
+CREATE TABLE `pendaftaran` (
+  `id_pendaftaran` int(11) NOT NULL,
+  `id_calon` int(11) NOT NULL,
+  `tanggal_pendaftaran` date NOT NULL,
+  `status_pendaftaran` enum('Menunggu','Diterima','Ditolak') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pengguna`
+--
+
+DROP TABLE IF EXISTS `pengguna`;
+CREATE TABLE `pengguna` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('Admin','Pengguna') NOT NULL,
+  `id_calon` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pengumuman`
+--
+
+DROP TABLE IF EXISTS `pengumuman`;
+CREATE TABLE `pengumuman` (
+  `id_pengumuman` int(11) NOT NULL,
+  `id_calon` int(11) NOT NULL,
+  `tanggal_pengumuman` date NOT NULL,
+  `status` enum('Diterima','Tidak Diterima','','') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `seleksi`
+--
+
+DROP TABLE IF EXISTS `seleksi`;
+CREATE TABLE `seleksi` (
+  `id_seleksi` int(11) NOT NULL,
+  `id_calon` int(11) NOT NULL,
+  `hasil_tes` decimal(10,0) NOT NULL,
+  `nilai_wawancara` decimal(10,0) NOT NULL,
+  `status_seleksi` enum('Lulus','Tidak Lulus','','') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `calon_siswa`
+--
+ALTER TABLE `calon_siswa`
+  ADD PRIMARY KEY (`id_calon`);
+
+--
+-- Indeks untuk tabel `dokumen_pendaftaran`
+--
+ALTER TABLE `dokumen_pendaftaran`
+  ADD PRIMARY KEY (`id_dokumen`),
+  ADD KEY `id_calon` (`id_calon`);
+
+--
+-- Indeks untuk tabel `orang_tua`
+--
+ALTER TABLE `orang_tua`
+  ADD PRIMARY KEY (`id_orang_tua`),
+  ADD KEY `id_calon` (`id_calon`);
+
+--
+-- Indeks untuk tabel `pendaftaran`
+--
+ALTER TABLE `pendaftaran`
+  ADD PRIMARY KEY (`id_pendaftaran`),
+  ADD KEY `id_calon` (`id_calon`),
+  ADD KEY `id_calon_2` (`id_calon`);
+
+--
+-- Indeks untuk tabel `pengguna`
+--
+ALTER TABLE `pengguna`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_calon` (`id_calon`);
+
+--
+-- Indeks untuk tabel `pengumuman`
+--
+ALTER TABLE `pengumuman`
+  ADD PRIMARY KEY (`id_pengumuman`),
+  ADD KEY `id_calon` (`id_calon`);
+
+--
+-- Indeks untuk tabel `seleksi`
+--
+ALTER TABLE `seleksi`
+  ADD PRIMARY KEY (`id_seleksi`),
+  ADD KEY `id_calon` (`id_calon`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `calon_siswa`
+--
+ALTER TABLE `calon_siswa`
+  MODIFY `id_calon` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `dokumen_pendaftaran`
+--
+ALTER TABLE `dokumen_pendaftaran`
+  MODIFY `id_dokumen` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `orang_tua`
+--
+ALTER TABLE `orang_tua`
+  MODIFY `id_orang_tua` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `pendaftaran`
+--
+ALTER TABLE `pendaftaran`
+  MODIFY `id_pendaftaran` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `pengguna`
+--
+ALTER TABLE `pengguna`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `pengumuman`
+--
+ALTER TABLE `pengumuman`
+  MODIFY `id_pengumuman` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `seleksi`
+--
+ALTER TABLE `seleksi`
+  MODIFY `id_seleksi` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `dokumen_pendaftaran`
+--
+ALTER TABLE `dokumen_pendaftaran`
+  ADD CONSTRAINT `dokumen_pendaftaran_ibfk_1` FOREIGN KEY (`id_calon`) REFERENCES `calon_siswa` (`id_calon`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `orang_tua`
+--
+ALTER TABLE `orang_tua`
+  ADD CONSTRAINT `orang_tua_ibfk_1` FOREIGN KEY (`id_calon`) REFERENCES `calon_siswa` (`id_calon`) ON DELETE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `pendaftaran`
+--
+ALTER TABLE `pendaftaran`
+  ADD CONSTRAINT `pendaftaran_ibfk_1` FOREIGN KEY (`id_calon`) REFERENCES `calon_siswa` (`id_calon`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `pengguna`
+--
+ALTER TABLE `pengguna`
+  ADD CONSTRAINT `pengguna_ibfk_1` FOREIGN KEY (`id_calon`) REFERENCES `calon_siswa` (`id_calon`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `pengumuman`
+--
+ALTER TABLE `pengumuman`
+  ADD CONSTRAINT `pengumuman_ibfk_1` FOREIGN KEY (`id_calon`) REFERENCES `calon_siswa` (`id_calon`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `seleksi`
+--
+ALTER TABLE `seleksi`
+  ADD CONSTRAINT `seleksi_ibfk_1` FOREIGN KEY (`id_calon`) REFERENCES `calon_siswa` (`id_calon`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
