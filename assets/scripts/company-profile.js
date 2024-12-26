1f4c7ccb24d05e4e487a4ff8622c278fc1b0c536
rezdni <reza.dani@raharja.info>
2024-12-27 03:29:31 +0700

    update

--- assets/scripts/company-profile.js
+++ assets/scripts/company-profile.js
@@ -24,6 +24,12 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
+            // active sections for animation on scroll
+            sec.classList.add('show-animate');
+        }
+        // if want to use animation that repeats on scroll use this
+        else {
+            sec.classList.remove('show-animate');
         }
     });
     // sticky header
@@ -34,4 +40,9 @@ window.onscroll = () => {
     // remove toggle icon and navbar when click navbar Links (scroll)
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
+
+    // animation footer on scroll 
+    let footer = document.querySelector('footer');
+
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 }
