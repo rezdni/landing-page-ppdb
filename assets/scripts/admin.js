0d16bab47903f53af52a218e105f459dc35bee44
fatahillah2606 <muhamad.fatahillah@raharja.info>
2024-12-29 15:15:38 +0700

    Creating a news feature

--- assets/scripts/admin.js
+++ assets/scripts/admin.js
@@ -1,3 +1,23 @@
+// Pengguna
+
+// Tampilkan pengguna
+function showUsers() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "../../backend/kelola-pengguna.php?listPengguna=true", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            let listPengguna = document.getElementById("list-pengguna");
+            listPengguna.innerHTML = xhr.responseText;
+        } else {
+            alert("Failed to process data");
+        }
+    }
+    
+    xhr.send();
+}
+
 // Hapus akun pengguna
 function hapusPengguna(idPengguna, namaPengguna) {
     if (confirm("Anda yakin ingin menghapus akun " + namaPengguna + "?")) {
@@ -28,4 +48,105 @@ function hapusPengguna(idPengguna, namaPengguna) {
         // Kirim request ke back-end
         xhr.send("removeUser=true&userId=" + encodeURIComponent(idPengguna));
     }
+}
+
+// Berita
+
+// Tombol form berita
+let formBerita = document.getElementById("buat-berita");
+function toggleBuatBerita() {
+    if (formBerita.classList.contains("show")) {
+        formBerita.classList.remove("show")
+    } else {
+        formBerita.classList.add("show");
+    }
+}
+
+// list berita
+function listBerita() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "../../backend/kelola-pengumuman.php?listPengumuman=true", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            let listBerita = document.querySelector(".list-berita");
+            listBerita.innerHTML = xhr.responseText;
+        } else {
+            alert("Failed to process data");
+        }
+    }
+    
+    xhr.send();
+}
+
+// simpan berita
+function simpanBerita(elmn, event) {
+    event.preventDefault();
+
+    let judulBerita = elmn.querySelector("#judul-berita").value;
+    let isiBerita = elmn.querySelector("#isi-berita").value;
+
+    // buat koneksi xhttprequest
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "../../backend/kelola-pengumuman.php", true);
+    xhr.setRequestHeader(
+        "Content-Type",
+        "application/x-www-form-urlencoded"
+    );
+
+    // terima respon back-end
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            let respon = JSON.parse(xhr.responseText);
+            if (respon.status === "berhasil") {
+                alert("Berita berhasil dibuat");
+                listBerita();
+                toggleBuatBerita();
+            } else {
+                alert("Kesalahan dalam menghapus akun");
+                console.log(respon.keterangan);
+            }
+        } else {
+            alert("Kesalahan dalam menghubungkan ke server");
+        }
+    };
+
+    xhr.send(
+        "buatBerita=true&judulBerita=" +
+        encodeURIComponent(judulBerita) +
+        "&isiBerita=" +
+        encodeURIComponent(isiBerita)
+    );
+}
+
+// Hapus berita
+function hapusBerita(idBerita) {
+        // buat koneksi xhttprequest
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", "../../backend/kelola-pengumuman.php", true);
+        xhr.setRequestHeader(
+            "Content-Type",
+            "application/x-www-form-urlencoded"
+        );
+
+        // terima respon back-end
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                let respon = JSON.parse(xhr.responseText);
+                if (respon.status === "berhasil") {
+                    alert("Berita berhasil dihapus");
+                    listBerita();
+                } else {
+                    alert("Kesalahan dalam menghapus berita");
+                    console.log(respon.keterangan);
+                }
+            } else {
+                alert("Kesalahan dalam menghubungkan ke server");
+            }
+        };
+
+        // Kirim request ke back-end
+        xhr.send("hapusBerita=true&idBerita=" + encodeURIComponent(idBerita));
+    
 }
---
+++ backend/kelola-pengumuman.php
@@ -0,0 +1,73 @@
+<?php
+require_once "../config/koneksi.php";
+
+// Buat berita
+if (isset($_POST["buatBerita"])) {
+    // variable
+    $judulBerita = htmlspecialchars($_POST["judulBerita"]);
+    $isiBerita = htmlspecialchars($_POST["isiBerita"]);
+
+    try {
+        // Prepare query
+        $insertSql = "INSERT INTO pengumuman (`id_pengumuman`, `judul`, `isi_pengumuman`) VALUES (null, :judulBerita, :isiBerita)";
+        $insertStmt = $pdo->prepare($insertSql);
+
+        $data = [
+            'judulBerita' => $judulBerita,
+            'isiBerita' => $isiBerita
+        ];
+        
+        // Insert to database
+        $insertStmt->execute($data);
+        echo json_encode(["status" => "berhasil"]);
+    } catch (PDOException $e) {
+        echo json_encode(["status" => "gagal", "keterangan" => $e]);
+    }
+}
+
+// List pengumuman
+if (isset($_GET["listPengumuman"])) {
+    // List user account
+    try {
+        $showSql = "SELECT * FROM pengumuman";
+        $stmt = $pdo->query($showSql);
+
+        // get result
+        $results = $stmt->fetchAll();
+        if (empty($results)) {
+            ?>
+                <h2>Data tidak tersedia</h2>
+            <?php
+        } else {
+            foreach ($results as $row) {
+                ?>
+                    <div class="berita hasil">
+                        <h3><?php echo $row["judul"]; ?></h3>
+                        <p><?php echo $row["isi_pengumuman"]; ?></p>
+                        <button name="hapus-berita" id="hapus-berita" onclick="hapusBerita('<?php echo $row['id_pengumuman']; ?>')">Hapus</button>
+                    </div>
+                <?php
+            }
+        }
+    } catch (PDOException $th) {
+        echo json_encode(["status" => "gagal", "keterangan" => $e]);
+    }
+}
+
+// hapus akun
+if (isset($_POST["hapusBerita"])) {
+    $idPengumuman = htmlspecialchars($_POST["idBerita"]);
+
+    try {
+        $deleteSql = "DELETE FROM pengumuman WHERE id_pengumuman = :idPengumuman";
+        $stmt = $pdo->prepare($deleteSql);
+
+        $deleteParam = ["idPengumuman" => $idPengumuman];
+
+        $stmt->execute($deleteParam);
+        echo json_encode(["status" => "berhasil"]);
+    } catch (PDOException $e) {
+        echo json_encode(["status" => "gagal", "keterangan" => $e]);
+    }
+}
+?>
