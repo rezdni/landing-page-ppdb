c2d2f489c3f5decf81977cedbfa4578f58110271
rezdni <reza.dani@raharja.info>
2025-01-06 19:15:45 +0700

    Update

--- assets/scripts/popup.js
+++ assets/scripts/popup.js
@@ -1,16 +1,16 @@
-function createPopup(id){
+function createPopup(id) {
     let popupNode = document.querySelector(id);
     let overlay = popupNode.querySelector(".overlay");
-    let closeBtn = popupNode.querySelector(".closeBtn");
-    function openPopup(){
+    let closeBtn = popupNode.querySelector(".close-btn");
+    function openPopup() {
         popupNode.classList.add("active");
     }
-    function closePopup(){
+    function closePopup() {
         popupNode.classList.remove("active");
     }
-    overlay.addEvenListener("click", closePopup);
-    closBtn.addEvenListener("click", closePopup);
+    overlay.addEventListener("click", closePopup);
+    closeBtn.addEventListener("click", closePopup);
     return openPopup;
-};
+}
 
 let popup = createPopup(".popup");
