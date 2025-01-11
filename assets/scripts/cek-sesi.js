d0eb17ddc9b59833fda4fc48b24dee758fa866b9
fatahillah2606 <muhamad.fatahillah@raharja.info>
2025-01-11 20:31:26 +0700

    bug fixed on cek-sesi.js

--- assets/scripts/cek-sesi.js
+++ assets/scripts/cek-sesi.js
@@ -1,4 +1,6 @@
 function cekSesi(lokasi) {
+    let currentUrl = window.location.href;
+
     let xhrSesi = new XMLHttpRequest();
     xhrSesi.open("GET", "../../backend/cek-sesi.php", true);
     xhrSesi.setRequestHeader(
@@ -9,18 +11,34 @@ function cekSesi(lokasi) {
     xhrSesi.onload = function () {
         if (xhrSesi.status === 200) {
             const respon = JSON.parse(xhrSesi.responseText);
-            if (respon.diotentikasi && respon.role === "Admin") {
-                if (lokasi === "login") {
+
+            if (currentUrl.includes("login")) {
+                if (respon.diotentikasi && respon.role === "Admin") {
                     window.location.href = "/views/admin/";
-                }
-            } else if (respon.diotentikasi && respon.role === "Calon") {
-                if (lokasi === "login") {
+                } else if (respon.diotentikasi && respon.role === "Calon") {
                     window.location.href = "/views/user/";
                 }
-            } else {
-                if (lokasi !== "login") {
+            } else if (respon.diotentikasi === false) {
+                if (
+                    currentUrl.includes("admin") ||
+                    currentUrl.includes("user")
+                ) {
                     window.location.href = "/views/login/";
                 }
+            }
+
+            if (
+                respon &&
+                respon.role === "Admin" &&
+                !currentUrl.includes("admin")
+            ) {
+                window.location.href = "/views/admin/";
+            } else if (
+                respon &&
+                respon.role === "Calon" &&
+                !currentUrl.includes("user")
+            ) {
+                window.location.href = "/views/user/";
             }
         }
     };
